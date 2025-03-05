e7d5a79117d3d0e0cb945ba53610739ac9f5a991
Nina Sproule <qut371@usask.ca>
2025-03-05 15:48:02 -0600

    Implement search page. Add lodash dependency for debouncing.

--- frontend/App.svelte
+++ frontend/App.svelte
@@ -12,6 +12,7 @@
 	import Home from './components/Home.svelte';
 	import Dashboard from './components/Dashboard.svelte';
 	import GradeCalculator from './components/GradeCalculator.svelte';
+	import Search from './components/Search.svelte';
 
 	// Check login status on mount
 	onMount(checkLoginStatus);
@@ -34,6 +35,9 @@
 		</Route>
 		<Route path="/calculator/:id" let:params>
 			<GradeCalculator id={params.id} />
+		</Route>
+		<Route path="/search">
+			<Search />
 		</Route>
 	</main>
 </Router>
