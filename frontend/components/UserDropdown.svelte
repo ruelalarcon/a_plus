64f48a0257ad66b90a9bbf02f8d89e2ecc4bc206
Nina Sproule <qut371@usask.ca>
2025-03-18 14:46:38 -0600

    Copied basic code for Dropdown and Avatar components from shadcn

---
+++ frontend/components/UserDropdown.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
+   </script>
+    
+   <DropdownMenu.Root>
+    <DropdownMenu.Trigger>Open</DropdownMenu.Trigger>
+    <DropdownMenu.Content>
+     <DropdownMenu.Group>
+      <DropdownMenu.Label>My Account</DropdownMenu.Label>
+      <DropdownMenu.Separator />
+      <DropdownMenu.Item>Profile</DropdownMenu.Item>
+      <DropdownMenu.Item>Settings</DropdownMenu.Item>
+      <DropdownMenu.Item>Log Out</DropdownMenu.Item>
+   
+     </DropdownMenu.Group>
+    </DropdownMenu.Content>
+   </DropdownMenu.Root>
+
+
